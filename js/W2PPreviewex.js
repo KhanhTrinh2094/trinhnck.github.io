c7d3f77a0c5cb47053571c845bc616bc23306d9a
Nguyen Canh Khanh Trinh <trinh.nguyen@thecoraledge.com>
2017-10-03 01:58:01 +0700

    Commit

---
+++ js/W2PPreviewex.js
@@ -0,0 +1,87 @@
+//////////////////////////////////////////
+// W2PPreviewex sample
+// Copyright(C) 2012 CREO Networks Co.,Ltd.<http://www.creo.co.jp> All Rights Reserved.
+//////////////////////////////////////////
+
+/////////////////////////////
+// Globals
+/////////////////////////////
+
+W2PPreviewex_onLoadFinished = function(resultId, detail) {
+	console.log("[W2PPreviewex_onLoadFinished()] " + resultId + ", " + detail);
+	W2PExternal_outDebug("[W2PPreviewex_onLoadFinished()] " + resultId + ", " + detail);
+	w2pPreviewex.onLoadFinished(resultId, detail);
+};
+
+W2PPreviewex_onValidateFinished = function(resultId, detail) {
+	console.log("[W2PPreviewex_onValidateFinished()] " + resultId + ", " + detail);
+	W2PExternal_outDebug("[W2PPreviewex_onValidateFinished()] " + resultId + ", " + detail);
+	w2pPreviewex.onValidateFinished(resultId, detail);
+};
+
+W2PPreviewex_onSaveFinished = function(resultId, detail) {
+	console.log("[W2PPreviewex_onSaveFinished()] " + resultId + ", " + detail);
+	W2PExternal_outDebug("[W2PPreviewex_onSaveFinished()] " + resultId + ", " + detail);
+	w2pPreviewex.onSaveFinished(resultId, detail);
+};
+
+
+/////////////////////////////
+// W2PPreviewex class
+/////////////////////////////
+
+var W2PPreviewex = function(swfId, callbackFunction) {
+	this.swfId = swfId;
+	this.swfObj = null;
+	this.callbacks = new Array();
+	this.loadCallbackFunction = callbackFunction;
+	this.validateCallbackFunction = null;
+	this.saveCallbackFunction = null;
+};
+
+W2PPreviewex.prototype = new W2PExternalBase();
+
+W2PPreviewex.prototype.validate = function(callbackFunction) {
+	console.log("[W2PPreviewex.validate()]");
+	W2PExternal_outDebug("[W2PPreviewex.validate()]");
+	var ret = false;
+	try {
+		this.validateCallbackFunction = callbackFunction;
+		var obj = this.getSwfObj();
+		if (obj) {
+			obj.validate();
+			ret = true;
+		}
+	} catch (e) {
+	}
+	return ret;
+};
+
+W2PPreviewex.prototype.onValidateFinished = function(resultId, details) {
+	W2PExternal_outDebug("[W2PPreviewex.onValidateFinished()] " + resultId);
+	console.log("[W2PPreviewex.onValidateFinished()] " + resultId);
+	if ((this.validateCallbackFunction != null) && (typeof this.validateCallbackFunction == "function")){
+		this.validateCallbackFunction.call(this, resultId, details);
+	}
+};
+
+W2PPreviewex.prototype.save = function(callbackFunction) {
+	console.log("[W2PPreviewex.save()]");
+	W2PExternal_outDebug("[W2PPreviewex.save()]");
+	var ret = false;
+	this.saveCallbackFunction = callbackFunction;
+	var obj = this.getSwfObj();
+	if (obj) {
+		obj.save();
+		ret = true;
+	}
+	return ret;
+};
+
+W2PPreviewex.prototype.onSaveFinished = function(resultId, detail) {
+	console.log("[W2PPreviewex.onSaveFinished()] " + resultId + ", " + detail);
+	W2PExternal_outDebug("[W2PPreviewex.onSaveFinished()] " + resultId + ", " + detail);
+	if ((this.saveCallbackFunction != null) && (typeof this.saveCallbackFunction == "function")){
+		this.saveCallbackFunction.call(this, resultId, detail);
+	}
+};
---
+++ js/hagaki2.js
@@ -0,0 +1,117 @@
+
+$(document).ready(function(){
+
+	$('#next').click(function() {
+		var ret = w2pPreviewex.validate(onValidateFinished);
+		if (ret) {
+			W2PExternal_outDebug("w2pPreviewex.validate() call succeeded");
+		} else {
+			showWarning("w2pPreviewex.validate() call failed");
+		}
+		return false;
+	});
+
+	$('#back').click(function() {
+		var ret = w2pPreviewex.save(onSaveFinishedAndBack);
+		if (ret) {
+			W2PExternal_outDebug("w2pPreviewex.save() call succeeded");
+		} else {
+			showWarning("w2pPreviewex.save() call failed");
+		}
+		return false;
+	});
+
+});
+
+function w2pPreviewex_onLoadFinished(resultId, detail) {
+	if (resultId = "I001") {
+	} else {
+		showWarning(resultId + ", " + detail);
+	}
+}
+
+function onValidateFinished(resultId, details) {
+	if (resultId == "I001") {
+		W2PExternal_outDebug("NOW save.");
+		var ret = w2pPreviewex.save(onSaveFinished);
+		if (ret) {
+			W2PExternal_outDebug("w2pPreviewex.save() call succeeded");
+		} else {
+			showWarning("w2pPreviewex.save() call failed");
+		}
+	} else {
+		var warn = "";
+		for (var i = 0; i < details.length; ++i) {
+			var arrDetails = details[i].split(',');
+			var itemName = arrDetails[3];
+			var message = arrDetails[1];
+			if ((itemName != null) && (itemName.length > 0)) {
+				warn += itemName + ":";
+			}
+			warn += message + "<br />";
+		}
+		showWarning(warn);
+	}
+}
+
+function onSaveFinished(resultId, detail) {
+	if (resultId == "I001") {
+		W2PExternal_outDebug("NOW submit.");
+		document.confirm.submit();
+	} else {
+		showWarning(resultId + ", " + detail);
+	}
+}
+
+function onSaveFinishedAndBack(resultId, detail) {
+	if (resultId == "I001") {
+		W2PExternal_outDebug("NOW submit.");
+		document.hagaki.submit();
+	} else {
+		showWarning(resultId + ", " + detail);
+	}
+}
+
+function onAddImageFinished(notifyId, details) {
+	if (notifyId == "I001") {
+		resetInfo();
+	} else {
+		var mesg = "画像の追加に失敗しました。";
+		if ((details != null) && (details.length > 0)) {
+			var detail = details[0];
+			if ((detail != null) && (detail.length > 0)) {
+				mesg += "：" + detail;
+			}
+		}
+		showWarning(mesg);
+	}
+}
+
+function onDeleteImageFinished(resultId, detail) {
+	if (resultId == "I001") {
+		resetInfo();
+	} else {
+		showWarning(resultId + ", " + detail);
+	}
+}
+
+function onResetHistoryFinished(resultId, detail) {
+	if (resultId == "I001") {
+		resetInfo();
+	} else {
+		showWarning(resultId + ", " + detail);
+	}
+}
+
+function resetInfo() {
+	W2PExternal_outDebug("[resetInfo()] ");
+	$('#warn').css('display', 'none');
+	$('#warn').html('');
+}
+
+function showWarning(mesg) {
+	W2PExternal_outDebug("[showWarning()] " + mesg);
+	$('#warn').css('display', 'block');
+	$('#warn').html(mesg);
+}
+
